be4472f8b07d1aaa77b8857228b6cd9aff4c0b10
Ivan Knyazev <knyazev13ivan@gmail.com>
2021-12-09 22:22:46 +0300

    add simple interface

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-import './style/style.css'
+// import './style/style.css'
+
+let field = document.querySelector('.game-field')
+let width = +/\d+/.exec(window.getComputedStyle(field).width)[0]
+let height = +/\d+/.exec(window.getComputedStyle(field).height)[0]
+
+let elem = document.querySelector('.elem')
+let elemSize = +/\d+/.exec(window.getComputedStyle(elem).width)[0]
+
+let btnRandom = document.querySelector('.control__random')
+let btnStart = document.querySelector('.control__start')
+let btnPlusOne = document.querySelector('.control__plus-one')
+let btnSpeed = document.querySelector('.control__speed')
+
+let matrix = new Array(width/elemSize).fill(new Array(height/elemSize))
+
+
+
+
+console.log(matrix)
