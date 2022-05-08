ee21e6cbd6f023717026a97654942b7cd6f1b226
Ivan Knyazev <knyazev13ivan@gmail.com>
2022-05-08 14:54:03 +0300

    add 4 presets

--- src/Components/StockPresets.ts
+++ src/Components/StockPresets.ts
@@ -1,3 +1,7 @@
 import { Glider } from "./Presets/Glider";
+import { LinePuffer } from "./Presets/LinePuffer";
+import { BlockSwitchEngine } from "./Presets/BlockSwitchEngine";
+import { F4 } from "./Presets/F4";
+import { MoreGliders } from "./Presets/MoreGliders";
 
-export const StockPresets: Array<[string, Array<Array<0 | 1>>]> = [Glider]
+export const StockPresets: Array<[string, Array<Array<0 | 1>>]> = [LinePuffer, BlockSwitchEngine, F4, MoreGliders, Glider]
